fefacf7a94678fe8414d12917da45ae18ac95cc6
Alchimis <79978300+Alchimis@users.noreply.github.com>
2025-01-31 23:50:11 +0300

    fix on acuracy formule

--- anal/accuracy.go
+++ anal/accuracy.go
@@ -13,5 +13,5 @@ func Accuracy() {
 type ConfusionMatrix [2][2]int
 
 func AccuracyConfusionMatrix(matrix ConfusionMatrix) float64 {
-	return (float64(matrix[0][0]) + float64(matrix[0][1])) / (float64(matrix[0][0]) + float64(matrix[0][1]) + float64(matrix[1][0]) + float64(matrix[1][1]))
+	return (float64(matrix[0][0]) + float64(matrix[1][1])) / (float64(matrix[0][0]) + float64(matrix[0][1]) + float64(matrix[1][0]) + float64(matrix[1][1]))
 }
